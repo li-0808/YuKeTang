a32bf6268f1c6aa5426f388533972917dc569f79
li-0808 <2859371427@qq.com>
2024-05-26 18:02:06 +0800

    优化代码结构

--- getClassSchedule.py
+++ getClassSchedule.py
@@ -1,31 +1,18 @@
-import requests
-from urllib.parse import quote
 from globalVar import Var
+from globalVar import headers_Host
+from getJson import get_json
 
 
-def get_json():
-    # 目标网站的URL
-    url = "https://xijing.yuketang.cn/mooc-api/v1/lms/learn/course/schedule"
 
-    # 需要发送的请求头
-    headers = {
-        "Host": "xijing.yuketang.cn",
-        "Cookie": Var.cookie,
-        "University-Id": "2679",
-        "Xtbz": "cloud",
-        # 根据需要添加其他头部信息
-    }
+def cache_mode(cid):
+    url = f"https://{headers_Host}/mooc-api/v1/lms/learn/course/schedule"
 
-    # 如果需要，这里可以添加请求的参数
     params = {
-        "cid": Var.cid,
-        "sign": "1",
-        # 根据需要添加其他参数
+        "cid": cid,
+        "sign": "1"
     }
 
-    # 发送GET请求
-    response = requests.get(url, headers=headers, params=params)
-
+    response = get_json(url, params)
     # 检查响应状态码
     if response.status_code == 200:
         # 打印响应的文本内容（假设返回的是JSON）
@@ -34,8 +21,6 @@ def get_json():
     else:
         print("Error:", response.status_code)
 
-
-def cache_mode():
     # 提取包含 total 和 done 字段的条目
     Var.classSchedule_work = [
         {id: {"total": details["total"], "done": details["done"]}}
