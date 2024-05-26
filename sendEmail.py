58f7ac4c0c10cd109ead83f6e458d7e120384e78
li-0808 <2859371427@qq.com>
2024-05-26 17:45:33 +0800

    更新邮件发送配置，优化代码结构，移除定时任务。

--- sendEmail.py
+++ sendEmail.py
@@ -1,7 +1,7 @@
 from globalVar import Var
+from globalVar import config
 import smtplib
 from email.mime.text import MIMEText
-import time
 import datetime
 
 
@@ -10,14 +10,15 @@ def send_email():
     today = datetime.datetime.today()
     today_str = f"{today.year}年{today.month}月{today.day}日"
 
-    # 将程序输出重定向到字符串变量
-
     # 设置邮件参数
-    sender = "2410419557@qq.com"
-    receiver = "2859371427@qq.com"
-    #receiver = "3239438965@qq.com"
+    sender = config['email']['sender']
+    receiver = config['email']['receiver']
     subject = f"{today_str}|作业完成情况"
     body = Var.sendemail_info
+    host = config['email']['host']
+    port = config['email']['port']
+    user = config['email']['user']
+    password = config['email']['password']
 
     # 创建邮件对象
     msg = MIMEText(body, "plain", "utf-8")
@@ -27,17 +28,11 @@ def send_email():
 
     # 发送邮件
     try:
-        smtp = smtplib.SMTP_SSL("smtp.qq.com", 465)
-        smtp.login("2410419557@qq.com", "ottftrkinsgmdjja")
+        smtp = smtplib.SMTP_SSL(host, port)
+        smtp.login(user, password)
         smtp.sendmail(sender, receiver, msg.as_string())
         smtp.quit()
         print("邮件发送成功")
     except Exception as e:
         print("邮件发送失败")
         print(e)
-
-# 设置定时任务
-# schedule.every().day.at("10:00").do(send_email)
-
-# while True:
-# schedule.run_pending()
